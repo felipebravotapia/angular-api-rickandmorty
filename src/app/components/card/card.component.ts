82187eb85cdbfcb8f0f07950ecfbd1ba45777a17
felipe30 <fbravo@pentafinanciero.cl>
2023-01-23 19:06:26 -0300

    fix manejo evento boton detalle

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  Input,
-  OnInit,
-  Renderer2,
-  EventEmitter,
-  Output,
-  AfterViewInit,
-} from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 import { Result } from 'src/app/interfaces/ICharacters';
 import { DataService } from '../../services/data.service';
 
@@ -16,21 +7,20 @@ import { DataService } from '../../services/data.service';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit, AfterViewInit {
+export class CardComponent implements OnInit {
   @Input() _dataCharacter!: Result[];
-  colors:string='';
+  colors: string = '';
   public removeEventListener!: () => void;
 
-  constructor(private renderer: Renderer2, private elementRef: ElementRef, private dataService: DataService) {}
+  constructor(
+    private renderer: Renderer2,
+    private elementRef: ElementRef,
+    private dataService: DataService
+  ) {}
 
   public handleAnchorClick(event: Event) {
     event.preventDefault();
-    const anchor = event.target as HTMLAnchorElement;
-    if (anchor.parentElement?.className.includes('mc-active')) {
-      anchor.parentElement?.classList.remove('mc-active');
-    } else {
-      this.renderer.addClass(anchor.parentElement, 'mc-active');
-    }
+    this.clickAnchor(event);
   }
 
   ngOnInit(): void {
@@ -45,12 +35,39 @@ export class CardComponent implements OnInit, AfterViewInit {
       }
     );
   }
-  ngAfterViewInit(): void {
 
-  }
-
-  onCallDetailse(id: number) {
+  onCallDetailse(id: number, event: any) {
+    this.clickIcon(event);
     this.dataService.viewDetailFn(id);
   }
 
+  clickIcon(event: any) {
+    const icon = event.target;
+
+    const Anchor = icon.parentElement;
+    const article = Anchor.parentElement.classList;
+    if (article.value.includes('mc-active')) {
+      /* icon.parentElement.classList
+        .removeClass('fa-bars')
+        .removeClass('fa-spin-fast')
+        .addClass('fa-arrow-left'); */
+      this.renderer.removeClass(Anchor.parentElement, 'mc-active');
+    } else {
+      icon.parentElement
+      /* icon.parentElement.classList.value
+        .removeClass('fa-arrow-left')
+        .removeClass('fa-spin-fast')
+        .addClass('fa-bars'); */
+      this.renderer.addClass(Anchor.parentElement, 'mc-active');
+    }
+  }
+
+  clickAnchor(event: Event) {
+    const anchor = event.target as HTMLAnchorElement;
+    if (anchor.parentElement?.className.includes('mc-active')) {
+      this.renderer.removeClass(anchor.parentElement, 'mc-active');
+    } else {
+      this.renderer.addClass(anchor.parentElement, 'mc-active');
+    }
+  }
 }
